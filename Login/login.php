4cf38eb255cc667725478f13ac0adb0f4a051f8b
guiokushi <guiokushi@hotmail.com>
2021-02-22 22:01:05 -0300

    Creation of the Login page
    add CSS
    add PHP page

---
+++ Login/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign-in</title>
+    <link rel="stylesheet" type="text/css" href="/Anime-Manga-DB/CSS/LOGIN_CSS.css">
+</head>
+<body>
+    <form>
+        <h2>Login</h2>
+    </form>
+</body>
+</html>
